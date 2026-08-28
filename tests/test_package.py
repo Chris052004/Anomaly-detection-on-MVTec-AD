9f010a34584f938a28a029bb2dce500c024041da
Christian <christian050104@gmail.com>
2026-08-28 09:15:00 +0200

    Aggiungo la struttura iniziale del progetto

---
+++ tests/test_package.py
@@ -0,0 +1,8 @@
+def test_anomaly_ae_is_importable():
+    import anomaly_ae
+    assert anomaly_ae is not None
+
+
+def test_scripts_is_importable():
+    import scripts
+    assert scripts is not None
